bc48ee35dde98c79c2b1cc64def4887b187975b6
hitalz <hitalz@icloud.com>
2017-09-08 17:33:15 -0500

    deleted testing commented area

--- include/add/reference.inc.php
+++ include/add/reference.inc.php
@@ -20,50 +20,6 @@
     VALUES ('$referenceCustomer', '$referenceSupplier', '$referenceArrivalDate', '$referenceArrivalTime', '$referenceCarrier', '$referencePurchaseOrder', '$referenceTrackingNumber', '$referencePackageQuantity', '$referencePackageType', '$referenceDescription')
     ";
 
-    // $runReferenceQuery = mysqli_query($conn, $addReferenceQuery);
-    if(mysqli_query($conn, $addReferenceQuery)) {
-      $customerQuery = "
-      SELECT *
-      FROM reference, customer, supplier, carrier, package
-      WHERE reference.customer_id = customer.customer_id AND reference.supplier_id = supplier.supplier_id AND reference.carrier_id = carrier.carrier_id AND reference.package_id = package.package_id
-      ORDER BY reference.reference_id
-      ";
-      $runCustomersQuery = mysqli_query($conn, $customerQuery);
-      $output .= '
-      <table class="table table-striped">
-        <thead>
-          <tr>
-          <th>Reference #</th>
-          <th>Customer</th>
-          <th>Supplier</th>
-          <th>Arrival Date</th>
-          <th>Arrival Time</th>
-          <th>P.O.</th>
-          <th>Carrier</th>
-          <th>Tracking Number</th>
-          <th>No. Packages</th>
-          <th>Type</th>
-          </tr>
-        </thead>';
-      while($row = mysqli_fetch_array($runCustomersQuery)) {
-        $output .= '
-        <tbody>
-          <tr>
-            <td>' . $row["reference_id"] . '</td>
-            <td>' . $row["customer_name"] . '</td>
-            <td>' . $row["supplier_name"] . '</td>
-            <td>' . $row["arrival_date"] . '</td>
-            <td>' . $row["arrival_time"] . '</td>
-            <td>' . $row["purchase_order"] . '</td>
-            <td>' . $row["carrier_name"] . '</td>
-            <td>' . $row["tracking_number"] . '</td>
-            <td>' . $row["package_quantity"] . '</td>
-            <td>' . $row["package_type"] . '</td>
-          </tr>
-        </tbody>';
-      }
-      $output .= '
-      </table>';
-    }
+    $runReferenceQuery = mysqli_query($conn, $addReferenceQuery);
   }
 ?>
